0c634978a2f297365155f6348e7245efa83569b4
AndreasDemenagas <ademenagas01@gmail.com>
2022-03-02 19:14:20 +0000

    Sample XML files & begun work on xml reading

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module seg.g33 {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires java.xml;
+
     opens seg.g33 to javafx.fxml;
     exports seg.g33;
 }
--- src/main/java/seg/g33/Helpers/XMLReading.java
+++ src/main/java/seg/g33/Helpers/XMLReading.java
@@ -1,4 +1,35 @@
 package seg.g33.Helpers;
 
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
 public class XMLReading {
+
+    public static void main(String[] args) {
+        var reader = new XMLReading();
+//        var name = XMLReading.class.getResource("Airport.xml").toExternalForm();
+
+        reader.configureAiportFromXMLFile("src/main/resources/Airport.xml");
+    }
+
+    public void configureAiportFromXMLFile(String filename) {
+        var factory = DocumentBuilderFactory.newInstance();
+        Document xmlFile;
+
+        try {
+            var builder = factory.newDocumentBuilder();
+            var file = new File(filename);
+
+            xmlFile = builder.parse(file);
+            xmlFile.getDocumentElement().normalize();
+
+            var root = xmlFile.getDocumentElement();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
