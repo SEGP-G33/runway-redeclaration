c8574c3828f4dfd282be7311ab52074752c242f4
Quintin Shaw <github@xyt.email>
2022-03-03 02:44:20 +0000

    print Calculator class in textual format

--- src/main/java/seg/g33/Helpers/TextPrint.java
+++ src/main/java/seg/g33/Helpers/TextPrint.java
@@ -1,9 +1,6 @@
 package seg.g33.Helpers;
 
-import seg.g33.Entitites.Airport;
-import seg.g33.Entitites.Obstacle;
-import seg.g33.Entitites.Plane;
-import seg.g33.Entitites.Runway;
+import seg.g33.Entitites.*;
 
 /**
  * ╺┳╸┏━╸╻ ╻╺┳╸┏━┓┏━┓╻┏┓╻╺┳╸
@@ -77,7 +74,7 @@ public class TextPrint {
                         "    |  _ \\| |      / \\  | \\ | | ____|\n" +
                         "    | |_) | |     / _ \\ |  \\| |  _|  \n" +
                         "    |  __/| |___ / ___ \\| |\\  | |___ \n" +
-                        "    |_|   |_____/_/   \\_\\_| \\_|_____|")
+                        "    |_|   |_____/_/   \\_\\_| \\_|_____|\n")
                 .append("  NAME\t\t\t:  " + plane.getName() + "\n")
                 .append("  BLAST\t\t:  " + plane.getBlastProtection() + "\n")
                 .append("-------- [System Information End] --------\n").toString();
@@ -85,11 +82,11 @@ public class TextPrint {
 
     public static String text(Obstacle obstacle) {
         StringBuilder string = new StringBuilder();
-        return string.append("     ____  _        _    _   _ _____ \n" +
-                        "    |  _ \\| |      / \\  | \\ | | ____|\n" +
-                        "    | |_) | |     / _ \\ |  \\| |  _|  \n" +
-                        "    |  __/| |___ / ___ \\| |\\  | |___ \n" +
-                        "    |_|   |_____/_/   \\_\\_| \\_|_____|")
+        return string.append("  ___  ____ ____ _____  _    ____ _     _____ \n" +
+                        " / _ \\| __ ) ___|_   _|/ \\  / ___| |   | ____|\n" +
+                        "| | | |  _ \\___ \\ | | / _ \\| |   | |   |  _|  \n" +
+                        "| |_| | |_) |__) || |/ ___ \\ |___| |___| |___ \n" +
+                        " \\___/|____/____/ |_/_/   \\_\\____|_____|_____|\n")
                 .append("  NAME\t\t\t:  " + obstacle.getName() + "\n")
                 .append("  HEIGHT\t\t:  " + obstacle.getHeight() + "\n")
                 .append("  CenterDistance\t:  " + obstacle.getCenterDistance() + "\n")
@@ -98,8 +95,25 @@ public class TextPrint {
                 .append("-------- [System Information End] --------\n").toString();
     }
 
+    public static String text(Calculator calculator) {
+        StringBuilder string = new StringBuilder();
+        return string.append("  ____    _    _     ____ _   _ _        _  _____ ___  ____  \n" +
+                        " / ___|  / \\  | |   / ___| | | | |      / \\|_   _/ _ \\|  _ \\ \n" +
+                        "| |     / _ \\ | |  | |   | | | | |     / _ \\ | || | | | |_) |\n" +
+                        "| |___ / ___ \\| |__| |___| |_| | |___ / ___ \\| || |_| |  _ < \n" +
+                        " \\____/_/   \\_\\_____\\____|\\___/|_____/_/   \\_\\_| \\___/|_| \\_\\\n")
+                .append("  NAME\t\t\t:  " + calculator.getName() + "\n")
+                .append("  HEIGHT\t\t:  " + calculator.getHeight() + "\n")
+                .append("  CenterDistance\t:  " + calculator.getCenterDistance() + "\n")
+                .append("  RightDistance\t:  " + calculator.getRightDistance() + "\n")
+                .append("  LeftDistance\t:  " + calculator.getLeftDistance() + "\n")
+                .append("-------- [System Information End] --------\n").toString();
+    }
+
+
+
 
     /**
-     * TODO: Obstacle; Calculate; RunwayParameters
+     * TODO: Calculate; RunwayParameters
      */
 }
