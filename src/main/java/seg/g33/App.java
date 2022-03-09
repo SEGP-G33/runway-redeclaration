15ccd6c43f07f573bd4a4c30053214b1dbe833dd
Jeffrey Sylvester <js13g19@soton.ac.uk>
2022-03-09 11:01:13 +0000

    feat: add title window to app

--- src/main/java/seg/g33/App.java
+++ src/main/java/seg/g33/App.java
@@ -17,6 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        stage.setTitle("Runway Redeclaration Calculator");
         scene = new Scene(loadFXML("launch"));
         stage.setScene(scene);
         stage.setTitle("Runway Redeclaration");
