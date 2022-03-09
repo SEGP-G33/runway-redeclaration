49424f191b9e0222ef79bb08a8acfa268783c81f
AndreasDemenagas <ademenagas01@gmail.com>
2022-03-09 14:13:38 +0000

    Added Umut's Comments

--- src/main/java/seg/g33/Entitites/Airport.java
+++ src/main/java/seg/g33/Entitites/Airport.java
@@ -1,29 +1,55 @@
 package seg.g33.Entitites;
 
 import java.util.ArrayList;
-import java.util.UUID;
 
 public final class Airport {
 
+    /**
+     * Name of the airport
+     */
     private String name;
+
+    /**
+     * ArrayList of runways
+     */
     private ArrayList<Runway> airportRunways;
 
+    /**
+     * Basic Constructor
+     */
     public Airport() {
         this.airportRunways = new ArrayList<Runway>();
     }
 
+    /**
+     * Basic Constructor
+     * @param name is the name of the airport
+     */
     public Airport(String name) {
         this.name = name;
     }
 
+    /**
+     * Adds a new runway to our list of runways
+     * @param runway is the new runway added
+     */
     public void addRunway(Runway runway){
         this.airportRunways.add(runway);
     }
 
+    /**
+     * Removes a new runway to our list of runways
+     * @param runway is the new runway removed
+     */
     public void removeRunway(Runway runway) {
         this.airportRunways.remove(runway);
     }
 
+    /**
+     * Retrieves a specific runway
+     * @param runwayName the name of the runway to be retrieved
+     * @return that retrieved runway
+     */
     public Runway retrieveRunway(String runwayName) {
         for (Runway runway : getAirportRunways()) {
             if (runway.getName().equals(runwayName)) {
@@ -33,11 +59,27 @@ public final class Airport {
         return null;
     }
 
+    /**
+     * Getter method to get the list of runways
+     * @return all added or updated runways in the airport
+     */
     public ArrayList<Runway> getAirportRunways() {
         return this.airportRunways;
     }
 
+    /**
+     * Getter method for the airport name
+     * @return the name of the airport
+     */
     public String getName() {
         return this.name;
+    }
+
+    /**
+     * Setter method to set the name of the airport
+     * @param name is the name of the airport to be set.
+     */
+    public void setName(String name) {
+        this.name = name;
     }
 }
