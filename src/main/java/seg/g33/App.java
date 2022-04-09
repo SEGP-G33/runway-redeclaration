714cdbf83a525d92a151b92f6e311febc9466ee5
AndreasDemenagas <ademenagas01@gmail.com>
2022-04-09 18:46:59 +0300

    Added presets for obstacles

--- src/main/java/seg/g33/App.java
+++ src/main/java/seg/g33/App.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import seg.g33.Helpers.Drawer;
 
 import java.io.IOException;
 
